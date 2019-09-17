0e65036554daa2cb8d8934af547f4bc475236cd8
BenjaminElizondo <34512679+BenjaminElizondo@users.noreply.github.com>
2019-09-17 12:54:36 -0400

    FIRST COMMIT

---
+++ script.js
@@ -0,0 +1,10 @@
+function setPixelColour(pixel){
+    pixel.style.backgroundColor = 'black';
+}
+var penColour = 'black';
+function setPenColour(pen){
+    penColour = pen;
+}
+function setPixelColour(pixel){
+    pixel.style.backgroundColor = penColour;
+}
